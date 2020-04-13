1e458cedeabaf03a493d15ffe5a90e5a66fa5480
Diego Stiehl <seccomiro@gmail.com>
2020-04-13 16:38:23 -0300

    Add integration test to test fetching from API

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Root from 'Root';
+import App from 'components/App';
+import moxios from 'moxios';
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('https://jsonplaceholder.typicode.com/posts', {
+    status: 200,
+    response: [{ title: 'Fetched #1' }, { title: 'Fetched #2' }],
+  });
+});
+afterEach(() => {
+  moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', (done) => {
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+  wrapped.find('.fetch-comments').simulate('click');
+  moxios.wait(() => {
+    wrapped.update();
+    expect(wrapped.find('li').length).toEqual(2);
+    done();
+    wrapped.unmount();
+  }, 100);
+});
