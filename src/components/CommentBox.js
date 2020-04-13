25f4fdab57574a0b6be51d22e3a4248f37ffda5c
Diego Stiehl <seccomiro@gmail.com>
2020-04-13 15:51:55 -0300

    Add a lot of test related stuff

---
+++ src/components/CommentBox.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { saveComment } from 'actions';
+
+class CommentBox extends React.Component {
+  state = { comment: '' };
+
+  handleChange = (e) => {
+    this.setState({ comment: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.saveComment(this.state.comment);
+    this.setState({ comment: '' });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea value={this.state.comment} onChange={this.handleChange} />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { saveComment })(CommentBox);
